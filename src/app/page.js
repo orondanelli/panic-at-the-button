1e1d8e44deec475440521f47651e6f933c89e8a5
orlando rondanelli <orlando.rondanelli@gmail.com>
2023-12-23 01:08:09 -0300

    test 24cevent

--- src/app/page.js
+++ src/app/page.js
@@ -1,23 +1,41 @@
 'use client'
-import { Card, Text, Button, Icon, Flex, Title, Grid } from "@tremor/react";
+import { Card, Text, Button, Icon, Flex, Title, Grid} from "@tremor/react";
 
-import { LinkIcon, BeakerIcon, ArrowRightIcon } from '@heroicons/react/24/solid'
+import { LightBulbIcon, LinkIcon, BeakerIcon, ArrowRightIcon } from '@heroicons/react/24/solid'
 
 const categories = [
+  {
+    title:"Enviar alerta",
+    text:"`¡Atención! se ejecutará la API de 24cevent.com!`",
+    action: {
+      via: "API",
+      method: "POST",
+      endpoint: "https://app.24cevent.com/api/clientapi/sendNotification",
+      body: {
+        "access_token": "ZZEdpGLrSntVruR8LEVi3DrCQMnKPZdLg4jtJv2STMDny2gF==",
+        "message": "Esta va desde el botón",
+        "language": "es-cl",
+        "servicio": "SERVICIO 1",
+        "criticidad": "critical",
+        "external_id": "custom_unique_identifier1",
+        "custom": {
+          "pais": "Argentina",
+          "negocio": "temp",
+          "Impacto": "Bajo"
+        }
+      }
+    },
+    icon:LightBulbIcon
+  },
   {
     title: "Pedir Pizza Hood",
     text: `Ordena tu pizza a domicilio ahora mismo dentro del condominio. ¡Entregas disponibles hasta las 22:00 horas! ¡Disfruta del sabor!"`,
     icon: BeakerIcon,
   },
   {
-    title: "Avisar que viene visita",
-    text: `¡Atención! Pronto llegará una visita. Por favor, permite su ingreso. Agradecemos tu colaboración. ¡Disfruta del encuentro!`,
-    icon: BeakerIcon,
-  },
-  {
     title: "Llamar a mamá",
     text: `¡Es hora de llamar a mamá! Marca y comparte ese momento especial. ¡Hazle saber cuánto la aprecias y extrañas!`,
-    icon: BeakerIcon,
+    icon: LightBulbIcon,
   },
   {
     title: "Enviar recordatorio de medicamento",
@@ -26,7 +44,33 @@ const categories = [
   },
 ];
 
+const takeAction = async (action) => {
+  try {
+    console.log(action.method)
+    console.log(action.endpoint)
+    console.log(action.body)
+
+    const response = await fetch(action.endpoint, {
+      method: action.method,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(action.body),
+    });
+
+    if (response.ok) {
+      console.log('Acción ejecutada exitosamente:', action);
+    } else {
+      console.error('Error al ejecutar la acción:', action);
+    }
+  } catch (error) {
+    console.error('Error al ejecutar la acción:', error);
+  }
+};
+
+
 export default function Home() {
+
   return (
     <main className="p-12">
       <Title>Panic at the button</Title>
@@ -39,8 +83,8 @@ export default function Home() {
           <Title className="mt-6">{item.title}</Title>
           <Text className="mt-2">{item.text}</Text>
           <Flex className="mt-6 pt-4 border-t">
-            <Button size="xs" variant="light" icon={ArrowRightIcon} iconPosition="right">
-              View more
+            <Button size="xs" variant="light" icon={ArrowRightIcon} iconPosition="right" onClick={() => takeAction(item.action)}>
+              Ejecutar
             </Button>
           </Flex>
         </Card>
