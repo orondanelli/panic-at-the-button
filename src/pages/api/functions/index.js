8cf93312c121383e6e4b472b4a322f8a55db5a08
orlando rondanelli <orlando.rondanelli@gmail.com>
2023-12-23 15:45:27 -0300

    vercel functions

--- src/pages/api/functions/index.js
+++ src/pages/api/functions/index.js
@@ -2,7 +2,26 @@ export const config = {
   runtime: 'nodejs',
 };
 
+import { exec } from 'child_process';
+
 export default function handler(request, response) {
+
+  const command =
+    'curl -s -w "\n\n%{json}" "https://httpbin.org/json"'
+
+exec(command, { encoding: 'utf-8' }, (error, stdout, stderr) => {
+    if (error !== null) {
+        console.log('Error', error, stderr)
+
+        return
+    }
+
+    const [responseMetadata, response] = stdout.split('\n\n')
+
+    console.log('Metadata', JSON.parse(responseMetadata))
+    console.log('Response', JSON.parse(response))
+})
+  
   response.status(200).json({
     body: request.body,
     query: request.query,
